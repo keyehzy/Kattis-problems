11280e4914379f43beb280f4c9af0957bbd17a2a
Matheus Samuel Martins de Sousa <matheussamuelmdesousa@gmail.com>
2019-03-01 15:53:44 +0000

    Finished drmmessages.py thanks to Gbob

--- drmmessages.py
+++ drmmessages.py
@@ -1,35 +1,17 @@
-def drmmessages(alpha):
-    enc_message = input()
-    s1 = list(enc_message[:len(enc_message)//2])
-    s2 = list(enc_message[len(enc_message)//2:])
+def drmmessages():
+    enc = [ord(c) + 65 for c in input()]
 
-    p, q = rotate(s1, s2, alpha)
-    for i, c in enumerate(p):
-        k = (alpha[c] + alpha[q[i]]) % 26
-        p[i] = chr(k + 65)
+    val = sum(enc)
+
+    p, half = [], len(enc)//2
+    for j in range(half):
+        q = ((enc[j] + val + enc[j+half]) % 26 + 65)
+        p.append(chr(q))
 
     print(''.join(p))
     return
 
 
-def rotate(s1, s2, alpha):
-    rotation_value1 = 0
-    rotation_value2 = 0
-
-    for i in range(len(s1)):
-        rotation_value1 += alpha[s1[i]]
-        rotation_value2 += alpha[s2[i]]
-    
-    for i in range(len(s1)):
-        p = (alpha[s1[i]] + rotation_value1) % 26
-        q = (alpha[s2[i]] + rotation_value2) % 26
-        s1[i] = chr(p + 65)
-        s2[i] = chr(q + 65)
-    return s1, s2
-
-
 if __name__ == '__main__':
-    alpha =  {chr(i+65):i for i in range(0,26)} #alphabet dictionary
-    drmmessages(alpha)
-
+    drmmessages()
 
