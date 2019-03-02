cb9e986097dd16e70834ea65df96754f52c1547a
masamu <matheussamuelmdesousa@gmail.com>
2019-03-02 12:03:12 -0300

    Added icpawards.py.

--- icpcawards.py
+++ icpcawards.py
@@ -2,18 +2,18 @@ def icpcawards():
     n = int(input())
 
     unis = []
-    teams = []
+    c = 0
     for _ in range(n):
         u, t = map(str, input().split())
-        if u in unis:
-            if unis[unis.index(u)] < 1: 
-                unis[unis.index(u)] += 1
-                teams.append(t)
-            else:
-                continue
+        if u in unis or c > 11:
+            continue
         else:
             unis.append(u)
-            teams.append(t)
+            c += 1
+            print(u + ' ' + t)
+
+    return
+
+
 if __name__ == '__main__':
     icpcawards()
-
