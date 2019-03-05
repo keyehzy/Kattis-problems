d8b4fa4c1d2e476291abf9352980d0f63d5c40b6
masamu <matheussamuelmdesousa@gmail.com>
2019-03-05 16:06:58 -0300

    Started eligibility.py

---
+++ eligibility.py
@@ -0,0 +1,21 @@
+def eligibility():
+    n = int(input())
+    for _ in range(n):
+        name, second, born, c = map(str, input().split())
+        second = second.split(r'/')
+        born = born.split(r'/')
+
+        if int(second[0]) > 2010:
+            if int(born[0]) > 1991:
+                print(name + ' eligible')
+        elif int(c) > 41:
+            print(name + ' ineligible')
+        else:
+            print(name + ' coach petitions')
+
+    return
+
+
+if __name__ == '__main__':
+    eligibility()
+
