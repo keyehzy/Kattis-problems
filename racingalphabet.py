90d6057741f15c4da2acbfe5ff8f79c460115382
Matheus Samuel Martins de Sousa <matheussamuelmdesousa@gmail.com>
2019-03-05 14:53:06 -0300

    Almost racingalphabet.py

--- racingalphabet.py
+++ racingalphabet.py
@@ -1,19 +1,18 @@
 import math
 def racingthealphabet():
-    dx = 2.0 * math.pi * 30.0 / 27.0
+    arc = 2.0 * math.pi * 30.0 / 28.0
 
     n = int(input())
     for _ in range(n):
-
-        phrase = input().replace(' ', "[").replace("\'", "\\\\")
-        
+        phrase = input().replace(' ', "[").replace(r"'", r"\\")
         total = 0
         for j in range(len(phrase)-1):
             q = abs(ord(phrase[j]) - ord(phrase[j+1]))
-            qq = (65 + abs(ord(phrase[j]) + ord(phrase[j+1]))) % 28
-            total += min(q,qq)
-            print(total * dx / 15.0)
-        
+            forw = abs(ord(phrase[j]) - 92) + abs(ord(phrase[j+1]) - 64)
+            back = abs(ord(phrase[j]) - 64) + abs(ord(phrase[j+1]) - 92) 
+            total += min(q,forw,back) 
+
+        print('%.10f' % (len(phrase) + total * arc / 15.0))
     return
 
 
